99899aefeaf705e62181249f1d76a152ae358131
TheOctan <kanatush.tim@gmail.com>
2024-02-23 18:31:25 +0300

    Invert train movement

--- Assets/_Project/Scripts/TrainRider.cs
+++ Assets/_Project/Scripts/TrainRider.cs
@@ -8,7 +8,7 @@ namespace OctanGames
         [SerializeField] private float _startRotationSpeed = 100;
         [SerializeField] private float _maxRotationSpeed;
         [SerializeField] private float _acceleration = 10;
-        //[SerializeField] private bool _invertRotation = true;
+        [SerializeField] private bool _invertRotation = true;
 
         [Header("Components")]
         [SerializeField] private List<WheelJoint2D> _wheels;
@@ -16,7 +16,7 @@ namespace OctanGames
         private bool _movementStarted;
         private float _currentRotationSpeed;
 
-        private float Acceleration => _acceleration;
+        private float Acceleration => _invertRotation ? -_acceleration : _acceleration;
 
         private void FixedUpdate()
         {
@@ -27,14 +27,9 @@ namespace OctanGames
             else
             {
                 _currentRotationSpeed -= Acceleration;
-                if (false)
-                {
-                    _currentRotationSpeed = Mathf.Min(0, _currentRotationSpeed);
-                }
-                else
-                {
-                    _currentRotationSpeed = Mathf.Max(0, _currentRotationSpeed);
-                }
+                _currentRotationSpeed = _invertRotation
+                    ? Mathf.Min(0, _currentRotationSpeed)
+                    : Mathf.Max(0, _currentRotationSpeed);
             }
 
             ApplyWheelRotation(_currentRotationSpeed);
@@ -44,7 +39,12 @@ namespace OctanGames
         {
             _movementStarted = true;
 
-            ApplyWheelRotation(_startRotationSpeed);
+            if (Mathf.Approximately(_currentRotationSpeed, 0f))
+            {
+                ApplyWheelRotation(_invertRotation
+                    ? -_startRotationSpeed
+                    : _startRotationSpeed);
+            }
         }
 
         public void StopMovement()
