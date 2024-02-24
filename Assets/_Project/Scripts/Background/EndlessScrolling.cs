d752a9799067a2e7f3844bf98160ab8b47c279f2
TheOctan <kanatush.tim@gmail.com>
2024-02-24 21:04:09 +0300

    Fix parallax effect

--- Assets/_Project/Scripts/Background/EndlessScrolling.cs
+++ Assets/_Project/Scripts/Background/EndlessScrolling.cs
@@ -11,15 +11,23 @@ namespace OctanGames.Background
 
         private Vector2 _screenBounds;
         private Vector3 _lastCameraPosition;
+        private GameObject[] _donorLayers;
 
         private void Start()
         {
             _screenBounds = _viewCamera.ScreenToWorldPoint(
                 new Vector3(Screen.width, Screen.height, _viewCamera.transform.position.z));
 
-            foreach (GameObject layer in _layers)
+            _donorLayers = new GameObject[_layers.Length];
+            for (var i = 0; i < _layers.Length; i++)
             {
-                LoadChildObjects(layer);
+                GameObject layer = _layers[i];
+
+                _donorLayers[i] = new GameObject(layer.name + " (donor)");
+                _donorLayers[i].transform.SetParent(transform, true);
+                _donorLayers[i].transform.position = layer.transform.position;
+
+                LoadChildObjects(_layers[i], _donorLayers[i]);
             }
 
             _lastCameraPosition = _viewCamera.transform.position;
@@ -29,7 +37,7 @@ namespace OctanGames.Background
         {
             Vector3 cameraPosition = _viewCamera.transform.position;
 
-            foreach (GameObject layer in _layers)
+            foreach (GameObject layer in _donorLayers)
             {
                 MoveChildObjects(layer);
 
@@ -43,48 +51,48 @@ namespace OctanGames.Background
             _lastCameraPosition = cameraPosition;
         }
 
-        private void LoadChildObjects(GameObject parent)
+        private void LoadChildObjects(GameObject original, GameObject parent)
         {
-            var spriteRenderer = parent.GetComponent<SpriteRenderer>(); 
+            var spriteRenderer = original.GetComponent<SpriteRenderer>(); 
             float objectWidth = spriteRenderer.bounds.size.x;
             var childAmount = (int)Mathf.Ceil(_screenBounds.x * 2 / objectWidth);
-            GameObject copyParent = Instantiate(parent);
 
             for (var i = 0; i <= childAmount; i++)
             {
-                GameObject c = Instantiate(copyParent, parent.transform);
+                GameObject c = Instantiate(original, parent.transform);
                 Vector3 parentPosition = parent.transform.position;
                 c.transform.position = new Vector3(objectWidth * i, parentPosition.y, parentPosition.z);
-                c.name = parent.name + i;
+                c.name = original.name + i;
             }
-            Destroy(copyParent);
-            Destroy(spriteRenderer);
+            original.SetActive(false);
         }
 
         private void MoveChildObjects(GameObject parent)
         {
-            Transform[] childObjects = parent.GetComponentsInChildren<Transform>();
-            if (childObjects.Length > 1)
+            int childCount = parent.transform.childCount;
+            if (childCount <= 1)
             {
-                GameObject firstChild = childObjects[1].gameObject;
-                GameObject lastChild = childObjects[childObjects.Length - 1].gameObject;
-                float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x;
+                return;
+            }
 
-                Vector3 cameraPosition = _viewCamera.transform.position;
-                if (cameraPosition.x + _screenBounds.x > lastChild.transform.position.x + halfObjectWidth - BORDER)
-                {
-                    firstChild.transform.SetAsLastSibling();
-                    firstChild.transform.position = new Vector3(lastChild.transform.position.x + halfObjectWidth * 2,
-                        lastChild.transform.position.y,
-                        lastChild.transform.position.z);
-                }
-                else if (cameraPosition.x - _screenBounds.x < firstChild.transform.position.x - halfObjectWidth + BORDER)
-                {
-                    lastChild.transform.SetAsFirstSibling();
-                    lastChild.transform.position = new Vector3(firstChild.transform.position.x - halfObjectWidth * 2,
-                        firstChild.transform.position.y,
-                        firstChild.transform.position.z);
-                }
+            GameObject firstChild = parent.transform.GetChild(0).gameObject;
+            GameObject lastChild = parent.transform.GetChild(childCount - 1).gameObject;
+            float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x;
+
+            Vector3 cameraPosition = _viewCamera.transform.position;
+            if (cameraPosition.x + _screenBounds.x > lastChild.transform.position.x + halfObjectWidth - BORDER)
+            {
+                firstChild.transform.SetAsLastSibling();
+                firstChild.transform.position = new Vector3(lastChild.transform.position.x + halfObjectWidth * 2,
+                    lastChild.transform.position.y,
+                    lastChild.transform.position.z);
+            }
+            else if (cameraPosition.x - _screenBounds.x < firstChild.transform.position.x - halfObjectWidth + BORDER)
+            {
+                lastChild.transform.SetAsFirstSibling();
+                lastChild.transform.position = new Vector3(firstChild.transform.position.x - halfObjectWidth * 2,
+                    firstChild.transform.position.y,
+                    firstChild.transform.position.z);
             }
         }
     }
