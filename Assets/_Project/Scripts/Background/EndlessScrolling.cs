38dab05448404a846454f1a978500714d2fc2af2
TheOctan <kanatush.tim@gmail.com>
2024-03-19 14:20:25 +0300

    Cach layer width

--- Assets/_Project/Scripts/Background/EndlessScrolling.cs
+++ Assets/_Project/Scripts/Background/EndlessScrolling.cs
@@ -12,7 +12,9 @@ namespace OctanGames.Background
         private Vector2 _screenBounds;
         private Vector3 _lastCameraPosition;
         private GameObject[] _donorLayers;
+
         private float[] _parallaxSpeedForLayer;
+        private float[] _layersWidth;
 
         private void Start()
         {
@@ -34,7 +36,7 @@ namespace OctanGames.Background
             for (var i = 0; i < _donorLayers.Length; i++)
             {
                 GameObject layer = _donorLayers[i];
-                MoveChildObjects(layer);
+                MoveChildObjectsInLayer(layer, i);
 
                 layer.transform.position += deltaMovement.x * _parallaxSpeedForLayer[i] * Vector3.right;
             }
@@ -45,15 +47,18 @@ namespace OctanGames.Background
         private void CalculateBackground()
         {
             _donorLayers = new GameObject[_layers.Length];
+            _layersWidth = new float[_layers.Length];
+
             for (var i = 0; i < _layers.Length; i++)
             {
-                GameObject layer = _layers[i];
+                var layer = _layers[i].GetComponent<SpriteRenderer>();
+                _layersWidth[i] = layer.bounds.size.x;
 
                 _donorLayers[i] = new GameObject(layer.name + " (donor)");
                 _donorLayers[i].transform.SetParent(transform, true);
                 _donorLayers[i].transform.position = layer.transform.position;
 
-                LoadChildObjects(_layers[i], _donorLayers[i]);
+                LoadChildObjects(layer, _donorLayers[i], _layersWidth[i]);
             }
         }
 
@@ -69,23 +74,21 @@ namespace OctanGames.Background
             }
         }
 
-        private void LoadChildObjects(GameObject original, GameObject parent)
+        private void LoadChildObjects(SpriteRenderer original, GameObject parent, float objectWidth)
         {
-            var spriteRenderer = original.GetComponent<SpriteRenderer>(); 
-            float objectWidth = spriteRenderer.bounds.size.x;
             var childAmount = (int)Mathf.Ceil(_screenBounds.x * 2 / objectWidth);
 
             for (var i = 0; i <= childAmount; i++)
             {
-                GameObject c = Instantiate(original, parent.transform);
+                SpriteRenderer c = Instantiate(original, parent.transform);
                 Vector3 parentPosition = parent.transform.position;
                 c.transform.position = new Vector3(objectWidth * i, parentPosition.y, parentPosition.z);
                 c.name = original.name + i;
             }
-            original.SetActive(false);
+            original.gameObject.SetActive(false);
         }
 
-        private void MoveChildObjects(GameObject parent)
+        private void MoveChildObjectsInLayer(GameObject parent, int layerNumber)
         {
             int childCount = parent.transform.childCount;
             if (childCount <= 1)
@@ -95,7 +98,7 @@ namespace OctanGames.Background
 
             GameObject firstChild = parent.transform.GetChild(0).gameObject;
             GameObject lastChild = parent.transform.GetChild(childCount - 1).gameObject;
-            float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x;
+            float halfObjectWidth = _layersWidth[layerNumber] * 0.5f;
 
             Vector3 cameraPosition = _viewCamera.transform.position;
             if (cameraPosition.x + _screenBounds.x > lastChild.transform.position.x + halfObjectWidth - BORDER)
